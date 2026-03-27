27dc00707f33e7a723eaeab65141f26396cfd645
t4pmgn7qqz-coder <t4pmgn7qqz@privaterelay.appleid.com>
2026-03-27 17:00:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for code in weather_codes:
 
 if will_rain:
     proxy_client = TwilioHttpClient()
-    proxy_client.session.proxies = {'https'L os.environ['https_proxy']}
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
     
     client = Client(account_sid, auth_token, http_client = proxy_client)
     message = client.messages.create(
